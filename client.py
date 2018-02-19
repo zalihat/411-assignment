5dec55f5643fae37814bf2f660ed154e3e420632
zalihat <36508180+zalihat@users.noreply.github.com>
2018-02-19 21:23:19 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,44 +1,33 @@
 import socket
 
 
-# create client socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# initialize and empty list to hold the message (an array of strings)
 message = []
 
-# ask user for the operation they want to perform
 operation = input(
     """
         Net-Centric Math Server
         
         Please select an operation to perform:
-        1 - Addidtion   2 - Subtraction     3 - Multiplication
+        1 - Addition   2 - Subtraction     3 - Multiplication
                     4 - Division    5 - Modulus
-        >_ 
     """
 )
 
-# add operation to message
 message.append(str(operation))
 
-# ask user for the two values on which the operation is performed
-first_variable = input('enter the first value: >_ ')
-second_variable = input('enter the second value: >_ ')
+firstInput = input('enter the first value: ')
+secongInput = input('enter the second value: ')
 
-# add the values to the message
-message.append(str(first_variable))
-message.append(str(second_variable))
+message.append(str(firstInput))
+message.append(str(secongInput))
 
-# format message into [operation,first_varible,second_variable]
 message = ','.join(message)
 
-# send message to client-side
-socket.sendto(message, ('localhost', 1998))
+socket.sendto(message, ('localhost', 5000))
 
-# receive result from server
-server_result, server_address = socket.recvfrom(1024*3)
-print('server result: ' + server_result)
+result, server = socket.recvfrom(1024*4)
+print('server result: ' + result)
 
-# close connection between client and server
 socket.close();
--- server.py
+++ server.py
@@ -1,46 +1,35 @@
 import socket
 
 
-# Create a TCP/IP socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Create server address
-server_address = ('localhost',  1998)
+server_address = ('localhost',  5000)
 print('Server waiting for connection')
 
-# Bind socket to the server_address
 socket.bind(server_address)
 
-# the math function
-def math(message):
-    # split message to remove the comms ','
+def calc(message):
     message = message.split(',')
 
-    # assign the values of the operation and variables
     operation = message[0]
-    first_variable = int(message[1])
-    second_variable = int(message[2])
+    firstInput = int(message[1])
+    secondInput = int(message[2])
 
-    # perform operation
     if operation == '1':
-        return first_variable + second_variable
+        return firstInput + secondInput
     elif operation == '2':
-        return first_variable - second_variable
+        return firstInput - secondInput
     elif operation == '3':
-        return first_variable * second_variable
+        return firstInput * secondInput
     elif operation == '4':
-        return first_variable / second_variable
+        return firstInput / secondInput
     elif operation == '5':
-        return first_variable % second_variable
+        return firstInput % secondInput
     else:
-        print('PLEASE USE A VALID OPERARION (add, subtract, multiply, divide or mod)')
+        print('Wrong operation. Use another')
 
-# wait for a connection using an infinite loopv
 while True:
     print('waiting for a connection...')
-    # receive message from client and the client address
-    client_message, client_address = socket.recvfrom(1024*3)
-    # compute the result
-    result = str(math(client_message))
-    # send result back to client
+    message, client_address = socket.recvfrom(1024*4)
+    result = str(calc(message))
     socket.sendto(result, client_address)
